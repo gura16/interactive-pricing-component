f8a93f946ce5412fdc2918551469de55c8a8dd87
gurami kvakhadze <gkvaxadze@gmail.com>
2023-04-17 19:44:16 +0100

    change javascript

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
-function rangeSlide(value) {
-  document.getElementById("rangevalue").innerHTML = "$" + value + ".00";
-}
-
 const button = document.querySelector(".togglebtn");
 const year = document.getElementById("year");
+const rangeInput = document.getElementById("rangevalue");
 let active = 0;
+let newvalue = 0;
+
+function rangeSlide(value) {
+  newvalue = value;
+  if (active == 0) {
+    rangeInput.innerHTML = "$" + newvalue + ".00";
+  } else {
+    rangeInput.innerHTML = `\$${newvalue * 12 - newvalue * 12 * 0.25}.00`;
+  }
+}
 
 button.addEventListener("click", function () {
   if (active == 0) {
     year.innerHTML = "/year";
+    rangeInput.innerHTML = `\$${newvalue * 12 - newvalue * 12 * 0.25}.00`;
     active = 1;
   } else {
     year.innerHTML = "/month";
     active = 0;
   }
 });
+
+let slider_color = document.querySelector(".range");
+slider_color.oninput = function () {
+  let value = ((this.value - this.min) / (this.max - this.min)) * 100;
+  this.style.background =
+    "linear-gradient(to right, #A4F3EB 0%, #A4F3EB " +
+    value +
+    "%, #ECF0FB " +
+    value +
+    "%, #ECF0FB 100%)";
+};
